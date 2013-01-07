4ce5da20f83c2615fb00f02409c00d57a23900b0
Butuzov <Butuzov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-07 11:10:26 +0000

    Tag 1.0 added
    git-svn-id: https://plugins.svn.wordpress.org/delpost/tags/0.1@649134 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,13 @@
+=== Plugin Name ===
+Contributors: butuzov
+Donate link: http://www.amazon.com/wishlist/2L9W5CCHAX8D0/
+Tags: attachments,performanse,optimization
+Requires at least: 2.2
+Tested up to: 2.5.1
+Stable tag: 0.1
+
+Whan you delete post  all related attachments not deleted from database and filesystem, this WordPress bug fixed by this plugin.
+
+== Description ==
+
+Whan you delete post  all related attachments not deleted from database and filesystem, this WordPress bug fixed by this plugin. 
---
+++ wp_deleteattachment.php
@@ -0,0 +1,33 @@
+<?php
+	/*
+	Plugin Name: Post Data Delete Advanced
+	Plugin URI: http://wordpress.org/extend/plugins/delpost
+	Description: This plugin delete attachments of deleted post and related databse records. Actualy this plugin is a fix for Wordpress.
+	Version: 0.1
+	Author: Oleg Butuzov
+	Author URI: http://made.ua/
+	*/
+
+
+
+	add_action('delete_post', 'deletepostdata');
+	
+	
+	function deletepostdata($id = 0){
+		global $wpdb;
+		
+		if ($id == 0 || $id === FALSE) 
+			return false;
+		if ( !current_user_can('delete_post', $id) ) 
+			return false;
+		
+		$res = get_children("post_parent=".intval($id)."&post_type=attachment&orderby=menu_order ASC, ID&order=DESC");
+		if (is_array($res)){
+			foreach($res as $array){
+				if (current_user_can('delete_post', $array->ID) ) {
+					wp_delete_attachment($array->ID);
+				}
+			}
+		}
+	}
+?>
